ed63948be29266818abcd94023351f4375602c4f
Denis Tsai <denistsai@aetheras.io>
2019-03-24 22:59:26 -0700

    added rust and cleaned up some vestigial code

--- install.sh
+++ install.sh
@@ -4,14 +4,23 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install base packages
-brew install go zsh zsh-completions kubectx hub
+brew install go zsh zsh-completions kubectx hub shfmt
 mkdir -p ~/go/bin
+
+# install rust
+curl https://sh.rustup.rs -sSf | sh
+# ios
+rustup target add aarch64-apple-ios armv7-apple-ios armv7s-apple-ios x86_64-apple-ios i386-apple-ios
+# android
+rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android
+# wasm
+rustup target add wasm32-unknown-unknown
 
 # install golang debugger
 go get -u github.com/derekparker/delve/cmd/dlv
 
-# install and setup antibody and terminal notifier
-brew install getantibody/tap/antibody 
+# install and setup antibody
+brew install getantibody/tap/antibody
 cp .zsh_plugins.txt ~/.zsh_plugins.txt
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
 
